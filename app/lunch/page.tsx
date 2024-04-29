d29368ca786ab990827d88fff5327d84f2d7c847
souldew <souldew2002@gmail.com>
2024-04-29 14:11:38 +0900

    create header

--- app/lunch/page.tsx
+++ app/lunch/page.tsx
@@ -1,25 +1,24 @@
 import MyTable from "../components/table";
 
 const lunchContents = [
-    {
-        date: "2024/04/01",
-        eat: "ラーメン",
-        dayOfHour: "12:00"
-    },
-    {
-        date: "2024/04/02",
-        eat: "hoge",
-        dayOfHour: "12:10",
-    },
+  {
+    date: "2024/04/01",
+    eat: "ラーメン",
+    dayOfHour: "12:00",
+  },
+  {
+    date: "2024/04/02",
+    eat: "hoge",
+    dayOfHour: "12:10",
+  },
 ];
 
-export default function Lunch() {
-    return (
-        <>
-            <MyTable 
-                data={lunchContents}
-            />
-        </>
+const hoge: string = "3";
 
-    )
+export default function Lunch() {
+  return (
+    <>
+      <MyTable foo={lunchContents} hoge={hoge} />
+    </>
+  );
 }
