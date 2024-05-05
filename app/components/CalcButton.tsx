6412dfcfca7aa3234d5fb43167850d52149ac788
souldew <souldew2002@gmail.com>
2024-05-05 14:31:56 +0900

    delete ununsed code

--- app/components/CalcButton.tsx
+++ app/components/CalcButton.tsx
@@ -1,9 +1,8 @@
-import { Button, Container } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 export default function DateOfItems() {
   return (
     <>
-      {/* <Button width={"60%"} mr={"20%"} ml={"20%"}> */}
       <Button width={"60%"} margin={"15px 20%"}>
         計算
       </Button>
